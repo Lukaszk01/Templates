23485d3b329d915dc60ba42811ec3ae1a4dad490
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-04-01 16:01:01 +0900

    sticky navbar works well

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-$(function() {
-    setTimeout(function() {
-        $(".navbar").addClass("sticky"); 
-    }, 2000);  
-});
+// STICKY NAVBAR WITH delay
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+        document.getElementById('navbar_top').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar_top').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
+
+
+// SLIDESHOW
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
